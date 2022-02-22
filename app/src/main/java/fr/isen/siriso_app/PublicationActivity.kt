14735e61ccebbbab06c8975a8c41040e1692a3e8
elise.rey <elise.rey@isen.yncrea.fr>
2022-02-22 11:32:50 +0100

    Add activities

---
+++ app/src/main/java/fr/isen/siriso_app/PublicationActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.siriso_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PublicationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publication)
+    }
+}
