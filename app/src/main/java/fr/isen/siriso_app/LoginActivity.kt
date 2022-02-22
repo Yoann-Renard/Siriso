762d781ad456f85449e15a1245e3adb4cf085794
elise.rey <elise.rey@isen.yncrea.fr>
2022-02-22 15:44:47 +0100

    firebase test

--- app/src/main/java/fr/isen/siriso_app/LoginActivity.kt
+++ app/src/main/java/fr/isen/siriso_app/LoginActivity.kt
@@ -1,11 +1,44 @@
 package fr.isen.siriso_app
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        lateinit var auth: FirebaseAuth
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_login)
+
+            auth = FirebaseAuth.getInstance()
+        }
+
+        override fun onStart() {
+            super.onStart()
+
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                //refresh
+            }
+        }
+
+        private fun createAccount(email: String, password: String) {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+                }
+        }
+
     }
-}
--- app/src/main/java/fr/isen/siriso_app/RegisterActivity.kt
+++ app/src/main/java/fr/isen/siriso_app/RegisterActivity.kt
@@ -2,10 +2,16 @@ package fr.isen.siriso_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
     }
 }
