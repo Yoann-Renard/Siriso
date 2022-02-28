ac17e51996d8167c986815a9d8679740c7d250a1
elise.rey <elise.rey@isen.yncrea.fr>
2022-02-28 13:15:44 +0100

    login, register, like button

--- app/src/main/java/fr/isen/siriso_app/LoginActivity.kt
+++ app/src/main/java/fr/isen/siriso_app/LoginActivity.kt
@@ -1,44 +1,54 @@
 package fr.isen.siriso_app
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import fr.isen.siriso_app.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-        lateinit var auth: FirebaseAuth
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_login)
+    lateinit var binding: ActivityLoginBinding
+    lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            auth = FirebaseAuth.getInstance()
+        binding.loginSignUp.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
 
-        override fun onStart() {
-            super.onStart()
-
-            val currentUser = auth.currentUser
-            if (currentUser != null) {
-                //refresh
-            }
-        }
-
-        private fun createAccount(email: String, password: String) {
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Log.d(TAG, "createUserWithEmail:success")
-                        val user = auth.currentUser
-                        //updateUI(user)
-                    } else {
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
-                        //updateUI(null)
-                    }
-                }
-        }
-
+        auth = FirebaseAuth.getInstance()
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+        //refresh
+        }
+    }
+
+    private fun createAccount(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    //updateUI(null)
+                }
+            }
+    }
+}
--- app/src/main/java/fr/isen/siriso_app/RegisterActivity.kt
+++ app/src/main/java/fr/isen/siriso_app/RegisterActivity.kt
@@ -1,16 +1,26 @@
 package fr.isen.siriso_app
 
+import android.content.Intent
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import fr.isen.siriso_app.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegisterBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerSignIn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
 
         auth = FirebaseAuth.getInstance()
     }
